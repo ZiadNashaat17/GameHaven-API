e6283e791b9db75160d9267321318a7a25903eb6
Ziad Nashaat <ziadnashaat03@gmail.com>
2025-11-06 10:57:33 +0200

    Refactor cart model schema and update pre-save middleware for total price calculation

--- models/cartModel.js
+++ models/cartModel.js
@@ -1,36 +1,44 @@
 import { Schema, model } from 'mongoose';
-import Item from './itemModel';
 
 const cartSchema = new Schema({
   user: {
-    type: Schema.ObjectId,
+    type: Schema.Types.ObjectId,
     ref: 'User',
     required: true,
+    unique: true,
   },
-  items: [Item],
-  totalQuantity: {
-    type: Number,
-    default: 0,
-  },
+  items: [
+    {
+      game: {
+        type: Schema.Types.ObjectId,
+        ref: 'Game',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        min: 1,
+        default: 1,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
   totalPrice: {
     type: Number,
     default: 0,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now(),
   },
 });
 
 // middleware to update the total quantity and total price
 cartSchema.pre('save', function (next) {
-  this.totalQuantity = this.items.reduce((acc, item) => acc + item.quantity, 0);
-  this.totalPrice = this.items.reduce((acc, item) => acc + item.quantity * item.price, 0);
-  this.updatedAt = Date.now();
+  this.items.forEach(item => {
+    let itemTotalPrice = item.quantity * item.price;
+    this.totalPrice = this.totalPrice + itemTotalPrice;
+    console.log('this pre save triggered');
+  });
+
   next();
 });
 
